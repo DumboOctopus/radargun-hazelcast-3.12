d05bb89f858a7d450cee40af76f93a3de5fb1d0a
Neil Prajapati <neilp0101@gmail.com>
2020-06-26 19:39:23 -0700

    removed num entries from gaussian and tested pipelined guassian locally

--- extensions/cache/src/main/java/org/radargun/stages/cache/test/GaussianKeysSelector.java
+++ extensions/cache/src/main/java/org/radargun/stages/cache/test/GaussianKeysSelector.java
@@ -13,14 +13,12 @@ import org.radargun.stages.test.TestStage;
  */
 public class GaussianKeysSelector implements KeySelector {
    private final Random random;
-   private final long numEntries;
    private final long mean;
    private final long standardDeviation;
    private final long minimum, maximum;
 
-   public GaussianKeysSelector(Random random, long numEntries, long mean, long standardDeviation, long minimum, long maximum) {
+   public GaussianKeysSelector(Random random, long mean, long standardDeviation, long minimum, long maximum) {
       this.random = random;
-      this.numEntries = numEntries;
       this.mean = mean;
       this.standardDeviation = standardDeviation;
       this.minimum = minimum;
@@ -43,13 +41,11 @@ public class GaussianKeysSelector implements KeySelector {
    @DefinitionElement(name = "gaussian-keys", doc = "In the test fixed set of entries is used and this is shared among all stressor threads. Additionally, the keys" +
       "are selected using a normal (gaussian) distribution.")
    public static class Factory implements KeySelectorFactory {
-      @Property(doc = "Total number of key-value entries.", optional = false)
-      private long numEntries = 0;
 
-      @Property(doc = "Mean value of indices. Default is numEntries/2.")
+      @Property(doc = "Mean value of indices. Default is 1 million")
       private long mean = -1;
 
-      @Property(doc = "Standard deviation. Default is numEntries/8.")
+      @Property(doc = "Standard deviation. Default is 100 thousand.")
       private long standardDeviation = -1;
 
       @Property(doc = "Minimum output. Default is mean-3*standard-deviation")
@@ -61,11 +57,11 @@ public class GaussianKeysSelector implements KeySelector {
       @Override
       public KeySelector newInstance(CacheOperationsTestStage stage, Random random, int globalThreadId, int threadId) {
          if (mean == -1) {
-            mean = numEntries / 2;
+            mean = 1_000_000;
          }
          if (standardDeviation == -1) {
             // over 99% of values are within 3 standard deviations http://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule
-            standardDeviation = numEntries / 8;
+            standardDeviation = 100_000;
          }
          // minimum == -1 means it wasn't set in the XML.
          if (minimum == -1) {
@@ -82,7 +78,7 @@ public class GaussianKeysSelector implements KeySelector {
             throw new IllegalArgumentException("Property maximum must be strictly greater than minimum for cache:gaussian-keys");
          }
 
-         return new GaussianKeysSelector(random, numEntries, mean, standardDeviation, minimum, maximum);
+         return new GaussianKeysSelector(random, mean, standardDeviation, minimum, maximum);
       }
 
       @Override
